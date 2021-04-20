328c0c72bd40bdaef50bbc8f2ee6df34bcc7ee34
isaeva anna <pangolin-666@mail.ru>
2021-04-20 18:26:12 +0300

    Test scaffolding

--- src/jepsen/etcdemo.clj
+++ src/jepsen/etcdemo.clj
@@ -1,38 +1,15 @@
 (ns jepsen.etcdemo
-  (:require [clojure.tools.logging :refer :all]
-            [clojure.string :as str]
-            [jepsen [cli :as cli]
-             [control :as c]
-             [db :as db]
-             [tests :as tests]]
-            [jepsen.control.util :as cu]
-            [jepsen.os.debian :as debian]))
-(def dir "/opt/etcd")
+  (:require [jepsen.cli :as cli]
+            [jepsen.tests :as tests]))
 
-(defn db
-  "Etcd DB for a particular version."
-  [version]
-  (reify db/DB
-    (setup! [_ test node]
-      (info node "installing etcd" version)
-      (c/su
-        (let [url (str "https://storage.googleapis.com/etcd/" version
-                       "/etcd-" version "-linux-amd64.tar.gz")]
-          (cu/install-archive! url dir))))
-
-    (teardown! [_ test node]
-      (info node "tearing down etcd"))))
 
 (defn etcd-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
-  :concurrency ...), constructs a test map."
+  :concurrency, ...), constructs a test map."
   [opts]
   (merge tests/noop-test
-         opts
-         {:name "etcd"
-          :os   debian/os
-          :db   (db "v3.1.5")
-          :pure-generators true}))
+         {:pure-generators true}
+         opts))
 
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
