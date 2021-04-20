cb7775d21e1276a7531de1e15c1a8396bb14d84b
isaeva anna <pangolin-666@mail.ru>
2021-04-20 18:16:57 +0300

    initial commit

---
+++ src/jepsen/etcdemo.clj
@@ -0,0 +1,42 @@
+(ns jepsen.etcdemo
+  (:require [clojure.tools.logging :refer :all]
+            [clojure.string :as str]
+            [jepsen [cli :as cli]
+             [control :as c]
+             [db :as db]
+             [tests :as tests]]
+            [jepsen.control.util :as cu]
+            [jepsen.os.debian :as debian]))
+(def dir "/opt/etcd")
+
+(defn db
+  "Etcd DB for a particular version."
+  [version]
+  (reify db/DB
+    (setup! [_ test node]
+      (info node "installing etcd" version)
+      (c/su
+        (let [url (str "https://storage.googleapis.com/etcd/" version
+                       "/etcd-" version "-linux-amd64.tar.gz")]
+          (cu/install-archive! url dir))))
+
+    (teardown! [_ test node]
+      (info node "tearing down etcd"))))
+
+(defn etcd-test
+  "Given an options map from the command line runner (e.g. :nodes, :ssh,
+  :concurrency ...), constructs a test map."
+  [opts]
+  (merge tests/noop-test
+         opts
+         {:name "etcd"
+          :os   debian/os
+          :db   (db "v3.1.5")
+          :pure-generators true}))
+
+(defn -main
+  "Handles command line arguments. Can either run a test, or a web server for
+  browsing results."
+  [& args]
+  (cli/run! (cli/single-test-cmd {:test-fn etcd-test})
+            args))
