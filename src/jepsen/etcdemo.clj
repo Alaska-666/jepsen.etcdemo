e778203606405d480585f0b74cf2b17919388a05
isaeva anna <pangolin-666@mail.ru>
2021-04-25 19:19:48 +0300

    Tunable difficulty

--- src/jepsen/etcdemo.clj
+++ src/jepsen/etcdemo.clj
@@ -14,46 +14,60 @@
             [knossos.model :as model])
   (:import (jepsen.etcdemo.client Client)))
 
-(defn etcd-test
-  "Given an options map from the command line runner (e.g. :nodes, :ssh,
-  :concurrency ...), constructs a test map.
-  Special options:
-       :quorum     Whether to use quorum reads"
-  [opts]
-  (let [quorum (boolean (:quorum opts))]
-    (merge tests/noop-test
-         opts
-         {:pure-generators true
-          :name            (str "etcd q=" quorum)
-          :quorum          quorum
-          :os              debian/os
-          :db              (db/db "v3.1.5")
-          :client          (Client. nil)
-          :nemesis         (nemesis/partition-random-halves)
-          :checker   (checker/compose
-                       {:perf  (checker/perf)
-                        :indep (independent/checker
-                                 (checker/compose
-                                   {:linear   (checker/linearizable {:model (model/cas-register)
-                                                                     :algorithm :linear})
-                                    :timeline (timeline/html)}))})
-          :generator  (->> (independent/concurrent-generator
-                             10
-                             (range)
-                             (fn [k]
-                               (->> (gen/mix [client/r client/w client/cas])
-                                    (gen/stagger 1/50)
-                                    (gen/limit 100))))
-                           (gen/nemesis
-                             (cycle [(gen/sleep 5)
-                                     {:type :info, :f :start}
-                                     (gen/sleep 5)
-                                     {:type :info, :f :stop}]))
-                           (gen/time-limit (:time-limit opts)))})))
 
 (def cli-opts
   "Additional command line options."
-  [["-q" "--quorum" "Use quorum reads, instead of reading from any primary."]])
+  [["-q" "--quorum" "Use quorum reads, instead of reading from any primary."]
+   ["-r" "--rate HZ" "Approximate number of requests per second, per thread."
+    :default  10
+    :parse-fn read-string
+    :validate [#(and (number? %) (pos? %)) "Must be a positive number"]]
+   [nil "--ops-per-key NUM" "Maximum number of operations on any given key."
+    :default  100
+    :parse-fn client/parse-long
+    :validate [pos? "Must be a positive integer."]]])
+
+
+(defn etcd-test
+  "Given an options map from the command line runner (e.g. :nodes, :ssh,
+  :concurrency ...), constructs a test map."
+  [opts]
+  (let [quorum (boolean (:quorum opts))]
+    (merge tests/noop-test
+           opts
+           {:pure-generators true
+            :name            (str "etcd q=" quorum)
+            :quorum          quorum
+            :os              debian/os
+            :db              (db/db "v3.1.5")
+            :client          (Client. nil)
+            :nemesis         (nemesis/partition-random-halves)
+            :checker         (checker/compose
+                               {:perf   (checker/perf)
+                                :indep (independent/checker
+                                         (checker/compose
+                                           {:linear   (checker/linearizable
+                                                        {:model (model/cas-register)
+                                                         :algorithm :linear})
+                                            :timeline (timeline/html)}))})
+            :generator       (->> (independent/concurrent-generator
+                                    10
+                                    (range)
+                                    (fn [k]
+                                      (->> (gen/mix [client/r client/w client/cas])
+                                           (gen/stagger (/ (:rate opts)))
+                                           (gen/limit (:ops-per-key opts)))))
+                                  (gen/nemesis
+                                    (->> [(gen/sleep 5)
+                                          {:type :info, :f :start}
+                                          (gen/sleep 5)
+                                          {:type :info, :f :stop}]
+                                         cycle))
+                                  )
+            }
+           )
+    )
+  )
 
 
 (defn -main
