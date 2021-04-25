6abe9af28d8b51cb7b33442d88f8aa11ff9d44b3
isaeva anna <pangolin-666@mail.ru>
2021-04-25 20:04:39 +0300

    added new options

--- src/jepsen/etcdemo.clj
+++ src/jepsen/etcdemo.clj
@@ -17,8 +17,35 @@
   (:import (jepsen.etcdemo.client Client)))
 
 
+(defn register-workload
+  "Tests linearizable reads, writes, and compare-and-set operations on
+  independent keys."
+  [opts]
+  {:client    (Client. nil)
+   :checker   (independent/checker
+                (checker/compose
+                  {:linear   (checker/linearizable {:model     (model/cas-register)
+                                                    :algorithm :linear})
+                   :timeline (timeline/html)}))
+   :generator (independent/concurrent-generator
+                10
+                (range)
+                (fn [k]
+                  (->> (gen/mix [client/r client/w client/cas])
+                       (gen/limit (:ops-per-key opts)))))})
+
+
+(def workloads
+  "A map of workload names to functions that construct workloads, given opts."
+  {"set"      set/workload
+   "register" register-workload})
+
+
 (def cli-opts
   "Additional command line options."
+  [["-w" "--workload NAME" "What workload should we run?"
+    :missing  (str "--workload " (cli/one-of workloads))
+    :validate [workloads (cli/one-of workloads)]]
   [["-q" "--quorum" "Use quorum reads, instead of reading from any primary."]
    ["-r" "--rate HZ" "Approximate number of requests per second, per thread."
     :default  10
@@ -36,57 +63,26 @@
 
       :quorum       Whether to use quorum reads
       :rate         Approximate number of requests per second, per thread
-      :ops-per-key  Maximum number of operations allowed on any given key."
+      :ops-per-key  Maximum number of operations allowed on any given key
+      :workload     Type of workload."
   [opts]
   (let [quorum    (boolean (:quorum opts))
-        workload  (set/workload opts)]
+        workload  ((get workloads (:workload opts)) opts)]
     (merge tests/noop-test
            opts
            {:pure-generators true
-            :name            (str "etcd q=" quorum)
-            :quorum          quorum
-            :os              debian/os
-            :db              (db/db "v3.1.5")
-            :client          (Client. nil)
-            :nemesis         (nemesis/partition-random-halves)
-            :checker         (checker/compose
-                               {:perf   (checker/perf)
-                                :indep (independent/checker
-                                         (checker/compose
-                                           {:linear   (checker/linearizable
-                                                        {:model (model/cas-register)
-                                                         :algorithm :linear})
-                                            :timeline (timeline/html)}))})
-            :generator       (->> (independent/concurrent-generator
-                                    10
-                                    (range)
-                                    (fn [k]
-                                      (->> (gen/mix [client/r client/w client/cas])
-                                           (gen/stagger (/ (:rate opts)))
-                                           (gen/limit (:ops-per-key opts)))))
-                                  (gen/nemesis
-                                    (->> [(gen/sleep 5)
-                                          {:type :info, :f :start}
-                                          (gen/sleep 5)
-                                          {:type :info, :f :stop}]
-                                         cycle))
-                                  (gen/time-limit (:time-limit opts)))}
-           {:client    (:client workload)
-            :checker   (:checker workload)
-            :generator (gen/phases
-                         (->> (:generator workload)
-                              (gen/stagger (/ (:rate opts)))
-                              (gen/nemesis
-                                (cycle [(gen/sleep 5)
-                                        {:type :info, :f :start}
-                                        (gen/sleep 5)
-                                        {:type :info, :f :stop}]))
-                              (gen/time-limit (:time-limit opts)))
-                         (gen/log "Healing cluster")
-                         (gen/nemesis (gen/once {:type :info, :f :stop}))
-                         (gen/log "Waiting for recovery")
-                         (gen/sleep 10)
-                         (gen/clients (:final-generator workload)))})))
+            :name       (str "etcd q=" quorum " "
+                             (name (:workload opts)))
+            :quorum     quorum
+            :os         debian/os
+            :db         (db/db "v3.1.5")
+            :nemesis    (nemesis/partition-random-halves)
+            :client     (:client workload)
+            :checker    (checker/compose
+                          {:perf     (checker/perf)
+                           :workload (:checker workload)})})
+    )
+  )
 
 
 (defn -main
